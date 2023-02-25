0c6d7896847a77ad4fddb5b16d8defe9596d933e
Maxwell Sultan <maxsultankent@gmail.com>
2023-02-24 22:08:13 -0700

    Add teams and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'teams/show'
+  get 'teams/new'
+  get 'teams/create'
+  get 'teams/update'
+  get 'teams/destroy'
+  root to: 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
